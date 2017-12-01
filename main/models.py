6b80f05cc9e51a4aed8d6f952d2ae56457114f22
Piotr Nosek <nosekpr@gmail.com>
2017-11-30 16:16:55 -0800

    Model postu; wyswietlanie na stronie glownej wszystkich postow posortowanych po dacie

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=300) 
+	content = models.CharField(max_length=10000)
+	publish_date = models.DateField()
+	user = models.ForeignKey(User)
+
+	def __str__(self):
+		return "Tytul: " + self.title + ", Zawartosc: " \
+		+ self.content + ", Data: " + self.publish_date.strftime("%d.%m.%Y") \
+		+ ", Autor:  " + self.user.username
+
+	class Meta:
+		ordering = ['-publish_date', 'title']
--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 def index(request):
-	return HttpResponse("Hello world")
+	posts = Post.objects.all()
+	context = {
+		'posts': posts
+	}
+	return render(request, 'main/index.html', context)
