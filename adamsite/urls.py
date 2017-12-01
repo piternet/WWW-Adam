c494c1284db6a0e5395120ec32185fc468574045
xyz <adamlitwin@interia.pl>
2017-12-01 09:03:13 +0100

    test commit

--- adamsite/urls.py
+++ adamsite/urls.py
@@ -1,4 +1,4 @@
-"""adamsite URL Configuration
+"""adamsite URL Configuration test
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
