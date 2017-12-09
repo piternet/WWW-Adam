0ef8f659469e3b2cadda0883a0258264ac4241b6
xyz <adamlitwin@interia.pl>
2017-12-09 20:58:25 +0100

    ManyToMany for Tag/Posts

--- main/models.py
+++ main/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Tag(models.Model):
+	name = models.CharField(max_length=200, blank=True, default='')
+
+	def __str__(self):
+		return self.name
+
+
 class Post(models.Model):
 	title = models.CharField(max_length=300) 
 	content = models.CharField(max_length=10000)
 	publish_date = models.DateField()
 	user = models.ForeignKey(User)
+	tag = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return "Tytul: " + self.title + ", Zawartosc: " \
@@ -15,5 +23,5 @@ class Post(models.Model):
 	class Meta:
 		ordering = ['-publish_date', 'title']
 
-class Tag(models.Model):
-	name = models.CharField(max_length=200)
+
+
