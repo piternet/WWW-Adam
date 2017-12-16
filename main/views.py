4f3102b33b7da1393f42c648df0b02a9c3c7240b
xyz <adamlitwin@interia.pl>
2017-12-16 23:38:31 +0100

    added onepost.html

--- main/views.py
+++ main/views.py
@@ -19,8 +19,11 @@ def tags(request):
 def tag_view(request, name):
 	tags = Tag.objects.all()
 	posts = Post.objects.filter(tag__name='#'+name)
-
 	context = {'posts':posts, 'name':name}
-
 	return render(request, 'main/tag1.html', context)
+
+def one_post(request, id):
+	post1 = Post.objects.get(id=id) #czemu nie dziala filter tutaj
+	context = {'post1':post1, 'id':id}
+	return render(request, 'main/onepost.html',context)
 
