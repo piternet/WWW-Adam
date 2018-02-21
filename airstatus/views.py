77060c0964765e165068387c59bc9a6fa8849fc5
xyz <adamlitwin@interia.pl>
2018-02-21 23:04:49 +0100

    added country and beginning city

--- airstatus/views.py
+++ airstatus/views.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from django.shortcuts import redirect
+from django.urls import reverse
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -19,7 +21,8 @@ def aircountry(request):
             for k, v in kr.items():
                 if k == 'name' and v == a:
                     countrycode = kr['code']
-        return HttpResponseRedirect(countrycode)
+        """return HttpResponseRedirect(countrycode, a)"""
+        return redirect('%s%s?name=' % (reverse('aircountry'),countrycode)+a)
     else:
         countryonly = []
         for kr in results:
@@ -31,26 +34,28 @@ def aircountry(request):
         }
         return render(request, 'main/air_country.html', context)
 
-def aircity(request, country):
+def aircity(request, country, **kwargs):
+    countryname = request.GET['name']
     oururl = "https://api.openaq.org/v1/cities"
     r = requests.get(oururl + "?country="+country)
     jdata = r.text
     results = json.loads(jdata)['results']
-    print(results)
     if request.method == "POST":
         a = request.POST['drop1']
-        for kr in results:
-            for k, v in kr.items():
+        for ct in results:
+            for k, v in ct.items():
                 if k == 'city' and v == a:
-                    countrycode = kr['code']
-        return HttpResponseRedirect(countrycode)
+                    citycode = ct['code']
+        return HttpResponseRedirect(citycode)
     else:
         cityonly = []
-        for kr in results:
-            for k, v in kr.items():
+        for ct in results:
+            for k, v in ct.items():
                 if k == 'city':
                     cityonly.append(v)
         context = {
-            'cityonly': cityonly
+            'cityonly': cityonly,
+            'countryname': countryname
+
         }
         return render(request, 'main/air_city.html', context)
